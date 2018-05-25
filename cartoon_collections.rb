a0d06a32376cce1f38055b5f496b61fd0b40d3e2
Nicolas Marini <nmarini44@gmail.com>
2018-05-25 14:21:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(array)# code an argument here
   array.each.with_index { |name, i| puts "#{i+1} #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map {|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
