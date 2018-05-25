ee271642440cc1b0efd05874f2a4699a015118cd
Nicolas Marini <nmarini44@gmail.com>
2018-05-25 14:44:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|name| name.size > 4 ? true : false }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|name| cheese_types.include?(name)}
 end
